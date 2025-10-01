94fa4037ed3bfc04bc5637f8071155a4139dfa97
FalfanJ <falfan.jos@hotmail.com>
2025-10-01 06:26:09 -0300

    Clases dominio agregadas

---
+++ Dominio/Vouchers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Vouchers
+    {
+        public int Id { get; set; }
+        public Clientes Cliente { get; set; }
+        public Articulos Articulo { get; set; }
+        public DateTime  FechaCanje { get; set; }
+    }
+}
